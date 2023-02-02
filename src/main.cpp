71c5018cd1c0d8f36370849cab5e517f388bc5b4
Manu <mr.willibald@live.de>
2023-02-02 12:49:20 +0100

    Add programming mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,31 @@
-#include "screen.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <EEPROM.h>
+#include "screen.h"
 #include "credentials.h"
 
+/*
 // Debug flag
 #ifndef DEBUG
 #define DEBUG
 #endif
+*/
 
 // WiFi credentials
-const char* ssid = STASSID;
-const char* password = STAPSK;
+const char *ssid = STASSID;
+const char *password = STAPSK;
 
 // Grinding times in ms
-#define SINGLE_SHOT_TIME 8700
-#define DOUBLE_SHOT_TIME 12600
+#define SINGLE_SHOT_TIME 6300
+#define DOUBLE_SHOT_TIME 9300
+typedef struct
+{
+  unsigned long singleShot = SINGLE_SHOT_TIME;
+  unsigned long doubleShout = DOUBLE_SHOT_TIME;
+} TShotTimes;
+TShotTimes times;
 
 // Update intervalls in ms
 #define PIN_INTERVAL 10
@@ -26,37 +35,30 @@ const char* password = STAPSK;
 #define FIRST_PAGE 0
 #define LAST_PAGE 1
 
-// update timestamps
-unsigned long lastPinUpdate = 0;
-unsigned long lastScreenUpdate = 0;
-unsigned long lastScreenRedraw = 0;
-boolean doScreenRedraw = false;
-
-// grinding
-unsigned long remainingGrindTime = 0;
-boolean grinding = false;
-
-// input pin states
-boolean lastGrindPin = false;
-boolean lastPageUpPin = false;
-boolean lastPageDownPin = false;
-
-// current screen
-int currentPage = 0;
-
 // current state
-struct
+typedef struct
 {
+  // program state
   boolean grinding = false;
   boolean programming = false;
-  byte page = 0;
+  byte page = FIRST_PAGE;
   unsigned long time = 0;
-} state;
-
+  boolean redrawScreen = false;
+  boolean updateScreen = false;
+  // update timestamps
+  unsigned long lastPinUpdate = 0;
+  unsigned long lastScreenUpdate = 0;
+  unsigned long lastScreenRedraw = 0;
+  // input pin states
+  boolean lastGrindPin = false;
+  boolean lastPageUpPin = false;
+  boolean lastPageDownPin = false;
+} TState;
+TState state;
 
 void OTA_init()
 {
-    // Port defaults to 8266
+  // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
 
   // Hostname defaults to esp8266-[ChipID]
@@ -69,7 +71,8 @@ void OTA_init()
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA.onStart([]() {
+  ArduinoOTA.onStart([]()
+                     {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -78,15 +81,13 @@ void OTA_init()
     }
 
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.println("Start updating " + type); });
+  ArduinoOTA.onEnd([]()
+                   { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
     Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
       Serial.println("Auth Failed");
@@ -95,11 +96,10 @@ void OTA_init()
     } else if (error == OTA_CONNECT_ERROR) {
       Serial.println("Connect Failed");
     } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
+      Serial.println("Receive Failed");      state.updateScreen = false;
     } else if (error == OTA_END_ERROR) {
       Serial.println("End Failed");
-    }
-  });
+    } });
   ArduinoOTA.begin();
 }
 
@@ -109,44 +109,44 @@ unsigned long get_grind_time(int page)
   unsigned long grindTime = 0;
   switch (page)
   {
-    case 0: grindTime = SINGLE_SHOT_TIME; break;
-    case 1: grindTime = DOUBLE_SHOT_TIME; break;
-    default: break;
+  case FIRST_PAGE:
+    grindTime = times.singleShot;
+    break;
+  case LAST_PAGE:
+    grindTime = times.doubleShout;
+    break;
+  default:
+    break;
   }
   return grindTime;
 }
 
-// update remaining grind time
-void update_remaining_grind_time(boolean grindActive, unsigned long lastUpdate, unsigned long* grindTime)
+void setup(void)
 {
-  if (grindActive)
-  {
-    *grindTime = *grindTime - (millis() - lastUpdate);
-  }
-  //return *grindTime;
-}
-
-// update new grind time
-void update_new_grind_time(boolean grindActive, unsigned long lastUpdate, unsigned long* grindTime)
-{
-  if (grindActive)
-  {
-    *grindTime = *grindTime + (millis() - lastUpdate);
-  }
-  //return *grindTime;
-}
-
-void setup(void) {
-    // serial init
+  // serial init
   Serial.begin(115200);
   Serial.println();
   Serial.println("Booting ...");
+
+#ifdef DEBUG
+  // put times to EEPROM
+  EEPROM.begin(16);
+  EEPROM.put(0, times);
+  EEPROM.commit();
+  EEPROM.end();
+#endif
+
+  // get times from EEPROM
+  EEPROM.begin(16);
+  EEPROM.get(0, times);
+  EEPROM.commit();
+  EEPROM.end();
 
   // pin init
   pinMode(D5, INPUT_PULLUP); // grinding request
   pinMode(D6, INPUT_PULLUP); // page up
   pinMode(D7, INPUT_PULLUP); // page down
-  pinMode(D8, OUTPUT);  // grinding output
+  pinMode(D8, OUTPUT);       // grinding output
 
   // init display
   init_screen();
@@ -154,7 +154,8 @@ void setup(void) {
   // start WIFI and connect
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
     ESP.restart();
@@ -163,16 +164,18 @@ void setup(void) {
   // start OTA
   OTA_init();
 
-  while (3000 > millis()) {};
+  while (3000 > millis())
+  {
+  };
 
   // first page
-  currentPage = 0;
-  grinding = false;
-  remainingGrindTime = SINGLE_SHOT_TIME;
-  draw_page(currentPage, grinding, remainingGrindTime);
-  lastScreenRedraw = millis();
-  lastScreenUpdate = lastScreenRedraw;
-  lastPinUpdate = millis();
+  state.page = FIRST_PAGE;
+  state.time = times.singleShot;
+  draw_page(state.page, state.grinding, state.time);
+  unsigned long now = millis();
+  state.lastScreenRedraw = now;
+  state.lastScreenUpdate = now;
+  state.lastPinUpdate = now;
 
   // summary
   Serial.println("Ready");
@@ -180,126 +183,145 @@ void setup(void) {
   Serial.println(WiFi.localIP());
 }
 
-void loop(void) {
+void loop(void)
+{
   // handle OTA
   ArduinoOTA.handle();
 
   // Pin update
-  if (millis() - lastPinUpdate > PIN_INTERVAL)
+  if (millis() - state.lastPinUpdate > PIN_INTERVAL)
   {
     // grind Pin D5
     boolean grindPinState = not digitalRead(D5);
-    if (grindPinState != lastGrindPin)
-    {
-      grinding = grindPinState;
-      lastGrindPin = grindPinState;
-    }
     // page up pin D6
     boolean pageUpPinState = not digitalRead(D6);
-    if (pageUpPinState != lastPageUpPin)
-    {
-      currentPage = currentPage + pageUpPinState;
-      if (1 < currentPage)
-      {
-        currentPage = 0;
-      }
-      if (pageUpPinState)
-      {
-        remainingGrindTime = get_grind_time(currentPage);
-      }
-      doScreenRedraw = pageUpPinState;
-      lastPageUpPin = pageUpPinState;
-    }
     // page down pin D7
     boolean pageDownPinState = not digitalRead(D7);
-    if (pageDownPinState != lastPageDownPin)
-    {
-      currentPage = currentPage - pageDownPinState;
-      if (0 > currentPage)
-      {
-        currentPage = 1;
-      }
-      if (pageDownPinState)
-      {
-        remainingGrindTime = get_grind_time(currentPage);
-      }
-      doScreenRedraw = pageDownPinState;
-      lastPageDownPin = pageDownPinState;
-    }
-    // calculate remaining grind time
-    update_remaining_grind_time(grinding, lastPinUpdate, &remainingGrindTime);
-    // reset grind time if time elapsed and update page
-    if (1000000 < remainingGrindTime)
-    {
-      grinding = false;
-      doScreenRedraw = true;
-      remainingGrindTime = 0;
-    }
-    // Redraw page if grind pin released
-    if ((0 == remainingGrindTime) && (not grindPinState))
-    {
-      doScreenRedraw = true;
-      remainingGrindTime = get_grind_time(currentPage);
-    }
 
     // update state diagram
-    if (grindPinState && not pageUpPinState && not pageDownPinState)
+    if ((grindPinState && not state.lastGrindPin && pageUpPinState && not pageDownPinState) ||
+        (grindPinState && not state.lastGrindPin && not pageUpPinState && pageDownPinState))
     {
-      state.grinding = true;
-      state.programming = false;
-      state.time = state.time - (millis() - lastPinUpdate);
-    }
-    else if ((grindPinState && pageUpPinState && not pageDownPinState) ||
-      (grindPinState && not pageUpPinState && pageDownPinState))
-    {
-      state.grinding = true;
+      // inititate programming mode
       state.programming = true;
-      state.time = state.time + (millis() - lastPinUpdate);
+      state.time = 0;
     }
-    else if (not grindPinState && not pageUpPinState && not pageDownPinState && lastPageUpPin)
+    else if (state.programming && not grindPinState && not pageUpPinState && not pageDownPinState && (state.lastPageUpPin || state.lastPageDownPin))
     {
+      // end programming mode
+      state.programming = false;
+      switch (state.page)
+      {
+      case FIRST_PAGE:
+        times.singleShot = state.time;
+        break;
+      case LAST_PAGE:
+        times.doubleShout = state.time;
+        break;
+      default:
+        break;
+      }
+      // write times to EEPROM
+      EEPROM.begin(16);
+      EEPROM.put(0, times);
+      EEPROM.commit();
+      EEPROM.end();
+
+      // redraw screen
+      state.redrawScreen = true;
+    }
+    else if (state.programming && grindPinState)
+    {
+      // program grinding time with counting up
+      state.grinding = true;
+      state.time = state.time + (millis() - state.lastPinUpdate);
+      state.updateScreen = true;
+    }
+    else if (state.programming && not grindPinState)
+    {
+      // still in programming but no grinding
+      state.grinding = false;
+    }
+    else if (grindPinState && (0 < state.time) && not state.programming && not pageUpPinState && not pageDownPinState)
+    {
+      // simple grinding with time counting down
+      state.grinding = true;
+      state.time = state.time - (millis() - state.lastPinUpdate);
+      state.updateScreen = true;
+      if (100000 < state.time)
+      {
+        state.time = 0;
+        state.grinding = false;
+        state.updateScreen = false;
+        state.redrawScreen = true;
+      }
+    }
+    else if (not state.grinding && not grindPinState && not pageUpPinState && not pageDownPinState && state.lastPageUpPin)
+    {
+      // page up
       (LAST_PAGE == state.page) ? state.page = FIRST_PAGE : state.page++;
+      state.time = get_grind_time(state.page);
+      state.redrawScreen = true;
     }
-    else if (not grindPinState && not pageUpPinState && not pageDownPinState && lastPageDownPin)
+    else if (not state.grinding && not grindPinState && not pageUpPinState && not pageDownPinState && state.lastPageDownPin)
     {
+      // page down
       (FIRST_PAGE == state.page) ? state.page = LAST_PAGE : state.page--;
+      state.time = get_grind_time(state.page);
+      state.redrawScreen = true;
+    }
+    else if (not grindPinState && state.lastGrindPin && (0 == state.time) && not state.programming && not pageUpPinState && not pageDownPinState)
+    {
+      state.time = get_grind_time(state.page);
+      state.redrawScreen = true;
+    }
+    else
+    {
+      state.grinding = false;
+      state.programming = false;
     }
 
+    // update last pin status
+    state.lastGrindPin = grindPinState;
+    state.lastPageUpPin = pageUpPinState;
+    state.lastPageDownPin = pageDownPinState;
+
     // set grinding pin
-    digitalWrite(D8, grinding);
-    lastPinUpdate = millis();
+    digitalWrite(D8, state.grinding);
+    state.lastPinUpdate = millis();
   }
 
   // Screen update
-  if (millis() - lastScreenUpdate > SCREEN_INTERVAL)
+  if (millis() - state.lastScreenUpdate > SCREEN_INTERVAL)
   {
     // Screen redraw
-    if (doScreenRedraw)
+    if (state.redrawScreen)
     {
-      draw_page(currentPage, grinding, remainingGrindTime);
-      #ifdef DEBUG
+      draw_page(state.page, state.grinding, state.time);
+#ifdef DEBUG
       Serial.print("Redraw interval: ");
-      Serial.println(millis() - lastScreenRedraw);
-      #endif
-      lastScreenRedraw = millis();
-      lastScreenUpdate = lastScreenRedraw;
-      doScreenRedraw = false;
+      Serial.println(millis() - state.lastScreenRedraw);
+#endif
+      state.lastScreenRedraw = millis();
+      state.lastScreenUpdate = state.lastScreenRedraw;
+      state.redrawScreen = false;
+      state.updateScreen = false;
     }
     // Screen update
     else
     {
-      update_page(currentPage, grinding, remainingGrindTime);
-      #ifdef DEBUG
+      update_page(state.page, state.grinding, state.time);
+#ifdef DEBUG
       Serial.print("Update interval: ");
-      Serial.println(millis() - lastScreenUpdate);
-      #endif
-      lastScreenUpdate = millis();
+      Serial.println(millis() - state.lastScreenRedraw);
+#endif
+      state.lastScreenUpdate = millis();
+      state.updateScreen = false;
     }
 
-    #ifdef DEBUG
-    Serial.print("Remaining grind time: ");
-    Serial.println(remainingGrindTime);
-    #endif
+#ifdef DEBUG
+    Serial.print("Current grind time: ");
+    Serial.println(state.time);
+#endif
   }
 }
-
